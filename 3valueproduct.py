0f21d8f12400ba9a8861630f816d7449585adc0e
ilhan48 <58470523+ilhan48@users.noreply.github.com>
2021-01-02 01:43:36 +0300

    Add files via upload

---
+++ 3valueproduct.py
@@ -0,0 +1,7 @@
+
+
+a = int(input("Please enter 1. value"))
+b = int(input("Please enter 2. value"))
+c = int(input("Please enter 3. value"))
+d = a * b * c
+print("Product of {},{},{} values: {}. ".format(a, b, c, d))
